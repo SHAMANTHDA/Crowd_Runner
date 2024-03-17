96ff6a4fc9d3de82073d3c8467739f2186fb1c72
SHAMANTH D A <88526736+SHAMANTHDA@users.noreply.github.com>
2024-03-17 19:39:33 +0530

    Initial commit

---
+++ Assets/Crowd Runner/Scripts/UIManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [Header(" Elements ")]
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject gamePanel;
+    [SerializeField] private GameObject gameoverPanel;
+    [SerializeField] private GameObject levelCompletePanel;
+
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private Text levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressBar.value = 0;
+
+        gamePanel.SetActive(false);
+        gameoverPanel.SetActive(false);
+
+        levelText.text = "Level " + (ChunkManager.instance.GetLevel() + 1);
+
+        GameManager.onGameStateChanged += GameStateChangedCallback;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.onGameStateChanged -= GameStateChangedCallback;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateProgressBar();
+    }
+
+    private void GameStateChangedCallback(GameManager.GameState gameState)
+    {
+        if (gameState == GameManager.GameState.Gameover)
+            ShowGameover();
+        else if (gameState == GameManager.GameState.LevelComplete)
+            ShowLevelComplete();
+    }
+
+    public void PlayButtonPressed()
+    {
+        GameManager.instance.SetGameState(GameManager.GameState.Game);
+
+        menuPanel.SetActive(false);
+        gamePanel.SetActive(true);
+    }
+
+    public void RetryButtonPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowGameover()
+    {
+        gamePanel.SetActive(false);
+        gameoverPanel.SetActive(true);
+    }
+
+    private void ShowLevelComplete()
+    {
+        gamePanel.SetActive(false);
+        levelCompletePanel.SetActive(true);
+    }
+
+    public void UpdateProgressBar()
+    {
+        if (!GameManager.instance.IsGameState())
+            return;
+
+        float progress = PlayerController.instance.transform.position.z / ChunkManager.instance.GetFinishZ();
+        progressBar.value = progress;
+    }
+}
---
+++ Assets/Crowd Runner/Scripts/VibrationManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerDetection.onDoorsHit += Vibrate;
+        Enemy.onRunnerDied += Vibrate;
+        GameManager.onGameStateChanged += GameStateChangedCallback;
+    }
+
+    private void OnDestroy()
+    {
+        PlayerDetection.onDoorsHit -= Vibrate;
+        Enemy.onRunnerDied -= Vibrate;
+
+        GameManager.onGameStateChanged -= GameStateChangedCallback;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GameStateChangedCallback(GameManager.GameState gameState)
+    {
+        if (gameState == GameManager.GameState.LevelComplete)
+            Vibrate();
+        else if (gameState == GameManager.GameState.Gameover)
+            Vibrate();
+    }
+
+    private void Vibrate()
+    {
+        Taptic.Light();
+    }
+}
